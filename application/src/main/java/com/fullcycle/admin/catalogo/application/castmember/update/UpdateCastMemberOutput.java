9922062150d5aae93b9b0213663d9afe0d773de7
Samuel Prazeres Junior <samuel.prazeresjr@bandtec.com.br>
2024-01-25 09:30:11 -0300

    adicionando api de castmember

--- application/src/main/java/com/fullcycle/admin/catalogo/application/castmember/update/UpdateCastMemberOutput.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/castmember/update/UpdateCastMemberOutput.java
@@ -1,6 +1,8 @@
 package com.fullcycle.admin.catalogo.application.castmember.update;
 
+import com.fullcycle.admin.catalogo.application.castmember.create.CreateCastMemberOutput;
 import com.fullcycle.admin.catalogo.domain.castmember.CastMember;
+import com.fullcycle.admin.catalogo.domain.castmember.CastMemberID;
 
 public record UpdateCastMemberOutput(
         String id
@@ -8,4 +10,8 @@ public record UpdateCastMemberOutput(
     public static UpdateCastMemberOutput from(final CastMember aMember){
         return new UpdateCastMemberOutput(aMember.getId().getValue());
     }
+    public static UpdateCastMemberOutput from(final CastMemberID anId) {
+        return new UpdateCastMemberOutput(anId.getValue());
+    }
+
 }
